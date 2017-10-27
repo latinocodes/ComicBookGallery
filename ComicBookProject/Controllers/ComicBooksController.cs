2ee77a0231f2610b09fa9fd2d584109ea0045058
Jose Lara <Jose@Joses-MacBook-Pro.local>
2017-10-27 17:35:52 -0400

    First Commit

---
+++ ComicBookProject/Controllers/ComicBooksController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using ComicBookGallery.Data;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+
+            return View(comicBook);
+        }
+    }
+}
